fe3109d3e71e75f4befd834d7f9763b3c382edb1
FelipeMaced0 <fmacedo746@gmail.com>
2024-04-18 00:34:50 -0300

    feat: removin duplicate tailwind classes

--- src/app/components/selectmenufunction.tsx
+++ src/app/components/selectmenufunction.tsx
@@ -22,7 +22,7 @@ export default function SelectMenu(props:any) {
     <Listbox value={selected} onChange={setSelected}>
       {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium leading-6 text-white-900 font-black">Function</Listbox.Label>
+          <Listbox.Label className="block text-sm font-medium leading-6 text-white-900">Function</Listbox.Label>
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
